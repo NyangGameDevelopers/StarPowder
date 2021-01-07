6c5c7d99752a8a9dc589ed68ff45fa4889c224c1
rito15 <ghdtkals15@naver.com>
2021-01-07 21:58:49 +0900

    Player Core
    Player Core Upload

---
+++ Assets/Scripts/Character/PlayerCore.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 2021-01-07 21:56
+
+/// <summary> 플레이어 캐릭터 핵심 컴포넌트 스크립트 </summary>
+public class PlayerCore : MonoBehaviour
+{
+    #region Public Fields
+
+
+
+    #endregion // ==========================================================
+
+    #region Private Fields
+
+    // Behavior Tree Root
+
+
+    #endregion // ==========================================================
+
+    #region Unity Events
+
+    private void Awake()
+    {
+        MakeNode();
+    }
+
+    #endregion // ==========================================================
+
+    #region Private Methods
+
+    /// <summary> BT 노드 조립 </summary>
+    private void MakeNode()
+    {
+
+    }
+
+    #endregion // ==========================================================
+
+    #region Public Methods
+
+
+
+    #endregion // ==========================================================
+
+}
