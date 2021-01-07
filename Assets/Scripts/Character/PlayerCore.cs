7acc4c404ee119c062a2598638ee85b6342c22fd
lacolico <52482311+lacolico@users.noreply.github.com>
2021-01-07 23:58:18 +0900

    cc
    캐릭터 움직임 변경

--- Assets/Scripts/Character/PlayerCore.cs
+++ Assets/Scripts/Character/PlayerCore.cs
@@ -21,21 +21,56 @@ public class PlayerCore : MonoBehaviour
     public Rigidbody RBody { get; private set; }
     public Animator Anim { get; private set; }
 
+
+    // Temp - CJK ***************************************************************
+    // Cam look variables.
+    [SerializeField]
+    private float rotSpeedX; // Mouse X sensitivity control, set in editor.
+    [SerializeField]
+    private float rotSpeedY; // Mouse Y sensitivity control, set in editor.
+    [SerializeField]
+    private float rotDamp; // Damping value for camera rotation.
+
+    private float mX = 0f; // Mouse X
+    private float mY = 0f; // Mouse Y
+
+    // Player move variables.
+    [SerializeField]
+    private float walkSpeed; // Walk (normal movement) speed, set in editor.
+    [SerializeField]
+    private float runSpeed; // Run speed, set in editor.
+
+    private float currentSpeed; // Stores current movement speed.
+
+    [SerializeField]
+    private KeyCode runKey; // Run key, set in editor.
+
+    private CharacterController cc; // Reference to attached CharacterController.
+
+    [SerializeField]
+    private GameObject playerCamera; // Player cam, set in editor.
+    // ***************************************************************
+
     #region Unity Events
 
     private void Awake()
     {
         MakeNode();
-
         RBody = GetComponent<Rigidbody>();
         if (RBody == null)
             RBody = gameObject.AddComponent<Rigidbody>();
         Anim = GetComponentInChildren<Animator>();
+
+        // Temp - CJK ***************************************************************
+        cc = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
+        // ***************************************************************
     }
 
     private void Update()
     {
-        TEMP_WasdMove();
+        TEMP_CJK_UpdateMove();
+        //TEMP_WasdMove();
     }
 
     #endregion // ==========================================================
@@ -80,6 +115,47 @@ public class PlayerCore : MonoBehaviour
         else if (_isRunning) Anim.Play("RUN");
         else Anim.Play("WALK");
     }
+
+    // Temp - CJK ***************************************************************
+    private void TEMP_CJK_UpdateMove()
+    {
+        // MOVEMENT - Hor and Ver axis:
+
+        // Get mouse axis
+        mX += Input.GetAxis("Mouse X") * rotSpeedX * (Time.deltaTime * rotDamp);
+        mY += Input.GetAxis("Mouse Y") * rotSpeedY * (Time.deltaTime * rotDamp) * -1;
+
+        // Clamp Y so player can't 'flip'
+        mY = Mathf.Clamp(mY, -80, 80); // mY 값이 범위를 넘지 않도록 (목이 돌아가지 않도록 설정)
+
+        // Adjust rotation of camera and player's body.
+        // Rotate the camera on its X axis for up / down camera movement.
+        playerCamera.transform.localEulerAngles = new Vector3(mY, 0f, 0f);
+        // Rotate the player's body on its Y axis for left/ right camera movement.
+        transform.eulerAngles = new Vector3(0f, mX, 0f);
+
+        // Get Hor and Ver imput.
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        // Set speed to walk speed.
+        currentSpeed = walkSpeed;
+        // If player is pressing run key and moving forward, set speed to run speed.
+        if (Input.GetKey(runKey) && Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = runSpeed;
+        }
+            
+
+        // Get new move position based off input.
+        Vector3 moveDir = (transform.right * hor) + (transform.forward * ver);
+        TEMP_UpdateAnimation(moveDir);
+
+        // Move CharController.
+        // Move will not apply gravity, use SimpleMove if you want gravity.
+        cc.Move(moveDir * currentSpeed * Time.deltaTime);
+    }
+    // ***************************************************************
 
     #endregion // ==========================================================
 
